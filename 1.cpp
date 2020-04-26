b03bbea924e234ccdebb5884b8b40d920060f531
JuaniMolina <64323040+JuaniMolina@users.noreply.github.com>
2020-04-26 01:17:51 -0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+    using namespace std;
+
+
+    int main() {
+        int n, c, i, p=0, tp=0;
+
+        for(c=1;c<=10;c++){
+            cout<<"Ingrese un numero: ";
+            cin>>n;
+                for(i=1;i<=n;i++){
+                    if(n%i==0) p++;
+                    }
+            if(p==2) tp++;
+            p=0;
+        }
+        cout<<endl <<tp;
+
+        return 0;
+}
